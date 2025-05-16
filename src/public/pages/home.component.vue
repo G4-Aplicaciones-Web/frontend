d2c03998dce26d86ef5fe81130c88cb5072e03c7
psfa29 <parodrez29@gmail.com>
2025-05-16 14:29:54 -0500

    feat: added tracking CRUD

---
+++ src/public/pages/home.component.vue
@@ -0,0 +1,20 @@
+<script>
+
+import {defineComponent} from "vue";
+import TrackingManagement from "@/tracking/pages/tracking-management.component.vue";
+
+
+export default defineComponent({
+  components: {TrackingManagement}
+})
+</script>
+
+<template>
+  <div>
+    <TrackingManagement/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
