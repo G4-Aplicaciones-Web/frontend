dbc945b76a3584127193520771eae43d4bfbe7c6
Mathias Bueno <mbuenope2506@gmail.com>
2025-05-16 16:25:46 -0500

    feat:agregando CRUD de meal_plans

---
+++ src/domains/meal_plans/pages/meal-plan-detail.component.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <!-- Detalles del meal plan -->
+    <MealPlanDay :mealPlanId="mealPlanId" />
+  </div>
+</template>
+
+<script setup>
+import MealPlanDay from '../components/meal-plan-day.component.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const mealPlanId = parseInt(route.params.id);
+</script>
