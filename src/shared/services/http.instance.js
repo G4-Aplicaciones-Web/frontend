5bf276caeea1938cf4428468a1fccd34d3bc0ea5
psfa29 <parodrez29@gmail.com>
2025-07-12 00:50:47 -0500

    feat: add css in tracking bounded context

--- src/shared/services/http.instance.js
+++ src/shared/services/http.instance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { authenticationInterceptor } from "@/domains/iam/services/authentication.interceptor.js";
+
 import { authenticationInterceptor } from "@/domains/iam/services/authentication.interceptor.js";
 
 const httpInstance = axios.create({
